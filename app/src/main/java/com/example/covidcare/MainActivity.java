aa2650e9df802182ed5533da876543f1c2332735
diaa <diaa.elsayedziada@gmail.com>
2020-05-29 01:27:55 +0200

    update menu names

--- app/src/main/java/com/example/covidcare/MainActivity.java
+++ app/src/main/java/com/example/covidcare/MainActivity.java
@@ -93,20 +93,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.item1:
-                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+            case R.id.meetingInfo:
+                Toast.makeText(this, "meeting Info selected", Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.item2:
-                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item3:
-                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.subtim1:
-                Toast.makeText(this, "Sub Item 1 selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.subtim2:
-                Toast.makeText(this, "Sub Item 2 selected", Toast.LENGTH_SHORT).show();
+            case R.id.updataStatus:
+                Toast.makeText(this, "updata Status selected", Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
